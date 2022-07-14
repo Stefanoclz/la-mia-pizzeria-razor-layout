3ea17e4cdea8b70b1596a0982279cf967f307f7e
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-14 14:12:14 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public string name = "";
+        public string description = "";
+        public string fotoLink = "";
+        public decimal prezzo;
+        public int id;
+
+        public Pizza(string name, string description, string fotoLink, decimal prezzo, int id)
+        {
+            this.name = name;
+            this.description = description;
+            this.fotoLink = fotoLink;
+            this.prezzo = prezzo;
+            this.id = id;
+        }
+
+    }
+}
